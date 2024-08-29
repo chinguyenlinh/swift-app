7769869fb580c9ca2955776511074b1b121e9318
Khang Nguyen Trong <s3929994@SGS104016-10.RMIT.EDU.VN>
2024-08-29 18:13:43 +0700

    init commit

---
+++ BlockBuster/BlockBusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockBusterApp.swift
+//  BlockBuster
+//
+//  Created by Khang Nguyen Trong on 29/8/24.
+//
+
+import SwiftUI
+
+@main
+struct BlockBusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
